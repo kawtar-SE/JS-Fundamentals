25c1e18884f1cc611161881c4a8661fdd75d1b31
kawtar assoulaimani <kawtarassoulaimani@gmail.com>
2025-09-19 22:52:48 +0100

    done

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const argc = process.argv.length;
+
+if (argc === 2) {
+  console.log("No argument");
+} else if (argc === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
